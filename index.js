4f9507093a2ff13a183d555cf562194c03c51d56
Chris Shaffer <chris@climbcredit.com>
2017-03-23 18:18:15 -0400

    validate ABA routing numbers

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  ABARoutingNumberIsValid: ABARoutingNumberIsValid
+};
+
+
+
+function ABARoutingNumberIsValid(routingNumberToTest) {
+  var routing = routingNumberToTest.toString();
+  while (routing.length < 9) {
+    routing = '0' + routing; //I refuse to import left-pad for this
+  }
+
+  //gotta be 9  digits
+  var match = routing.match("^\\d{9}$");
+  if (!match) {
+    return false;
+  }
+
+  //The first two digits of the nine digit RTN must be in the ranges 00 through 12, 21 through 32, 61 through 72, or 80.
+  //https://en.wikipedia.org/wiki/Routing_transit_number
+  const firstTwo = parseInt(routing.substring(0, 2));
+  const firstTwoValid =  (0 <= firstTwo && firstTwo <= 12)
+                      || (21 <= firstTwo && firstTwo <= 32)
+                      || (61 <= firstTwo && firstTwo <= 72)
+                      || firstTwo === 80;
+  if (!firstTwoValid) {
+    return false;
+  }
+
+  console.log('first two valid')
+
+  //this is the checksum
+  //http://www.siccolo.com/Articles/SQLScripts/how-to-create-sql-to-calculate-routing-check-digit.html
+  const weights = [3, 7 ,1];
+  var sum = 0;
+  for (var i=0 ; i<8; i++) {
+    sum += parseInt(routing[i]) * weights[i % 3];
+    console.log(sum)
+  }
+
+  return (10 - (sum % 10)) === parseInt(routing[8]);
+}
---
+++ test/index.js
@@ -0,0 +1,34 @@
+'use strict';
+const val = require('../index');
+const assert = require('assert');
+
+describe('ABA Routing Number Tests',function() {
+  it('Should fail an alphanumeric value',function() {
+    assert.equal(val.ABARoutingNumberIsValid('abcdacbde'), false);
+  });
+
+  it('Should fail a number greater that 9 digits',function() {
+    assert.equal(val.ABARoutingNumberIsValid('1234567890'), false);
+  });
+
+  it('Should pass this real bank routing number',function() {
+    assert.equal(val.ABARoutingNumberIsValid('021000021'), true);
+  });
+
+  it('Should pass this real bank routing number as an integer',function() {
+    assert.equal(val.ABARoutingNumberIsValid(322271627), true);
+  });
+
+  it('Dopes are going to make these ints so we should handle it',function() {
+    assert.equal(val.ABARoutingNumberIsValid(21000021), true);
+  });
+
+  it('Should fail this real-looking number (first two digits)',function() {
+    assert.equal(val.ABARoutingNumberIsValid(131000021), false);
+  });
+
+  it('Should fail this real-looking number (checksum)',function() {
+    assert.equal(val.ABARoutingNumberIsValid(322271628), false);
+  });
+
+});
